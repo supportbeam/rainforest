3b69a7da7bf554248057a0e3fff360a8fda7693a
JJLim <lim.jess9@gmail.com>
2015-02-08 17:56:04 -0500

    Creating review model and associating the user, product, and review models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,14 @@
 class Product < ActiveRecord::Base
+
 	validates :description, :name, presence: true # a new Product can only be created if both description and the name values are present
   validates :price_in_cents, numericality: {only_integer: true} # a new Product can only be created if price_in_cents in only an integer 
 
-#
- def formatted_price
-  price_in_dollars = price_in_cents.to_f / 100
-  sprintf("%.2f", price_in_dollars)
-end
+  has_many :reviews
+  has_many :users, through: :reviews
+
+  def formatted_price
+    price_in_dollars = price_in_cents.to_f / 100
+    sprintf("%.2f", price_in_dollars)
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
 
 	has_secure_password # automatically add attr_accessor :password, :password_confirmation in your model for you, which sets them up as virtual attributes (not database-backed)
 
+	has_many :reviews
+	has_many :products, through: :reviews
+
 end
