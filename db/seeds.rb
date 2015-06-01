53e5af307c78bf859a0ec8d8d621d5bb66c55e60
JJLim <lim.jess9@gmail.com>
2015-06-01 00:51:56 -0400

    Add faker gem and seed db with fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,28 @@
-Product.create!(
-  :name  => "Thermal Mug",
-  :description => "BPA free and keeps your coffee or tea hot!",
-  :price_in_cents    => 500
-)
+# Product.create!(
+#   :name  => "Thermal Mug",
+#   :description => "BPA free and keeps your coffee or tea hot!",
+#   :price_in_cents    => 500
+# )
 
-Product.create!(
-  :name  => "Glass Food Container",
-  :description => "BPA free and leak proof!",
-  :price_in_cents    => 1000
-)
+# Product.create!(
+#   :name  => "Glass Food Container",
+#   :description => "BPA free and leak proof!",
+#   :price_in_cents    => 1000
+# )
 
-Product.create!(
-  :name  => "Lip Balm",
-  :description => "All natural ingredients and leaves your lips supple soft!",
-  :price_in_cents    => 300
-)
+# Product.create!(
+#   :name  => "Lip Balm",
+#   :description => "All natural ingredients and leaves your lips supple soft!",
+#   :price_in_cents    => 300
+# )
+
+100.times do
+  Product.create({
+    name: Faker::Commerce.product_name, 
+    description: "#{Faker::Company.bs} from #{Faker::Company.name} #{Faker::Company.suffix}",
+    price_in_cents: rand(100000)
+    })
+end
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
